9b8edfc4c260c5ee63166e011998af991aebd7de
이준민JunminLee <91423541+leejunmin02@users.noreply.github.com>
2022-03-18 16:49:28 +0900

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,31 @@
+package week3;
+class TestEX{
+	private int a, b, c;
+
+	public TestEX(int a) {
+		super();
+		this.a = a;
+	}
+
+	public TestEX(int a, int b) {
+		this(a);
+		this.b = b;
+	}
+
+	public TestEX(int a, int b, int c) {
+		this(a, b);
+		this.c = c;
+		
+	}
+	
+	
+}
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+	}
+
+}
